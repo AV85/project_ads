e47f9a2c52b8e4a13265de3d1138488dcf7857cf
AV85 <sasha85ua@gmail.com>
2020-03-04 16:38:17 +0200

    added rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 User.destroy_all
-User.create!(email: 'admin@email.com', password: 'password', password_confirmation: 'password', first_name: 'John', last_name: 'Doe', role: 2)
+User.create!(email: 'admin@email.com',
+             password: 'password',
+             password_confirmation: 'password',
+             first_name: 'John',
+             last_name: 'Doe',
+             role: 2)
