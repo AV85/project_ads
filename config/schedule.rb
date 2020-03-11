b4b04c7100622ce523a5dd3f1e804855e995191e
AV85 <sasha85ua@gmail.com>
2020-03-11 12:15:12 +0200

    added whenever config and some fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,12 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+env :PATH, ENV['PATH']
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+set :environment, "development"
+set :output, {:standard => "log/cron_log.log", :error => "log/cron_error_log.log" }
 
-# Learn more: http://github.com/javan/whenever
+every 1.day, at: '0:00 am' do
+  runner "Cron.publish_all_approved_ads"
+end
+
+every 1.day, at: '11:50 am' do
+  runner "Cron.ads_to_archive"
+end
